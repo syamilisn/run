b19e5eb416f1bbb7f2f93e65fbab9a0a76bf761e
syamilisn <shyamilisn.itfa@gmail.com>
2023-02-23 17:47:59 +0530

    02/23/23

--- script.sh
+++ script.sh
@@ -10,6 +10,10 @@ echo "/*
 filename=`ls ${1}*`
 if [ `ls ${filename}` ];then
 	echo "${1} creation success."
+	echo "MODULE_AUTHOR("Syamili S N");
+	MODULE_DESCRIPTION("${2}");
+	MODULE_LICENSE("Dual MIT/GPL");
+	MODULE_VERSION("0.1");" >> ${filename}
 	cat run/template.c >> ${filename}
 else
 	echo "${1} creation failed."
